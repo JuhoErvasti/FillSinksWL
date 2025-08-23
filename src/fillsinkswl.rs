781e1a9ea774cc61dcc528c596ee239d2a01cfc2
JuhoErvasti <juho.ervasti@gispo.fi>
2025-08-23 13:39:27 +0300

    Get NDArray from input and write it as output, unchanged for now

--- src/fillsinkswl.rs
+++ src/fillsinkswl.rs
@@ -1,2 +1,36 @@
-struct FillSinks {
+struct Node {
+    x: i64,
+    y: i64,
+    spill: f64,
 }
+
+enum Direction {
+    North,
+    NorthEast,
+    East,
+    SouthEast,
+    South,
+    SouthWest,
+    West,
+    NorthWest,
+}
+
+pub struct FillSinks {
+    array: ndarray::Array2<f64>,
+}
+
+impl FillSinks {
+    pub fn new(array: ndarray::Array2<f64>) -> Self {
+        Self { array }
+    }
+
+    pub fn execute() {
+        todo!()
+    }
+
+    fn dir(x: i64, y: i64, z: f64) -> Direction {
+        todo!()
+    }
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::path::Path;
 
 use clap::{CommandFactory, Parser};
 
+// TODO: set GDAL error handler
+
 #[derive(Parser)]
 #[command(arg_required_else_help = true)]
 #[command(version, about, long_about = None)]
@@ -9,7 +11,7 @@ struct Cli {
     input: String,
     output: String,
 
-    // FIXME: should be > 0
+    // TODO: should be > 0
     #[arg(long = "minimum-slope", value_name = "MINIMUM_SLOPE", help = "Minimum Slope in degrees", long_help = "FIXME:")]
     minslope: Option<f64>,
 
@@ -35,12 +37,57 @@ fn main() {
         return ();
     }
 
-    println!(
-        "input: {}, output: {}, minslope: {}",
-        input.to_str().unwrap_or("SOMETHING WENT VERY WRONG"),
-        output.to_str().unwrap_or("SOMETHING WENT VERY WRONG"),
-        minslope,
-    );
+    let flags = gdal::GdalOpenFlags::GDAL_OF_RASTER | gdal::GdalOpenFlags::GDAL_OF_READONLY;
+    let options = gdal::DatasetOptions {
+        open_flags: flags,
+        allowed_drivers: None,
+        open_options: None,
+        sibling_files: None,
+    };
 
-    println!("{}", cli.overwrite);
+
+    let ds = match gdal::Dataset::open_ex(input, options) {
+        Ok(ds) => ds,
+        Err(err) => {
+            panic!("ERROR: {}", err.to_string());
+        },
+    };
+
+    if ds.raster_count() != 1 {
+        println!("ERROR: input must have exactly 1 band.");
+    }
+
+    if let Some(out_driver) = gdal::DriverManager::get_output_driver_for_dataset_name(output, gdal::DriverType::Raster) {
+        let in_band: gdal::raster::RasterBand = ds.rasterband(1).unwrap();
+        let in_buffer: gdal::raster::Buffer<f64> = in_band.read_band_as().unwrap();
+        let in_crs = ds.spatial_ref().unwrap();
+        let in_array: ndarray::Array2<f64> = in_buffer.to_array().unwrap();
+
+        let width = in_array.shape()[1];
+        let height = in_array.shape()[0];
+
+        let mut out_ds = out_driver.create_with_band_type::<f64, _>(
+            output,
+            width,
+            height,
+            1,
+        ).unwrap();
+
+        out_ds.set_spatial_ref(&in_crs).unwrap();
+        let mut out_band = out_ds.rasterband(1).unwrap();
+        let mut out_buffer = gdal::raster::Buffer::new(
+            (width, height), in_array.into_raw_vec_and_offset().0
+        );
+
+        out_band.write(
+            (0, 0),
+            (width, height),
+            &mut out_buffer,
+        ).unwrap();
+    } else {
+        println!("ERROR: could not determine an appropriate raster driver from {}", output.to_str().unwrap());
+        Cli::command().print_help().unwrap();
+        return ();
+    }
+
 }
