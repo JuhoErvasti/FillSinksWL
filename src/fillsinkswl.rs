2b9733e96e81b78b57159181d4b1228c9d3d07f6
JuhoErvasti <juho.ervasti@gispo.fi>
2025-08-23 23:44:55 +0300

    Initial implementation of the algorithm itself

--- src/fillsinkswl.rs
+++ src/fillsinkswl.rs
@@ -1,9 +1,40 @@
+use std::slice::Iter;
+
+#[derive(Debug)]
 struct Node {
-    x: i64,
-    y: i64,
-    spill: f64,
+    pub y: i64,
+    pub x: i64,
+    pub spill: f64,
 }
 
+impl Node {
+    fn new(y: i64, x: i64, spill: f64) -> Self {
+        Self { y, x, spill }
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.spill.total_cmp(&self.spill)
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(other.spill.total_cmp(&self.spill))
+    }
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.spill.total_cmp(&other.spill) == std::cmp::Ordering::Equal
+    }
+}
+
+impl Eq for Node {
+}
+
+#[derive(Debug)]
 enum Direction {
     North,
     NorthEast,
@@ -15,22 +46,123 @@ enum Direction {
     NorthWest,
 }
 
-pub struct FillSinks {
-    array: ndarray::Array2<f64>,
+impl Direction {
+    pub fn delta(&self, pos: (i64, i64)) -> (i64, i64) {
+        let (y, x) = pos;
+        match self {
+            Direction::North => (y - 1, x),
+            Direction::NorthEast => (y - 1, x + 1),
+            Direction::East => (y, x + 1),
+            Direction::SouthEast => (y + 1, x + 1),
+            Direction::South => (y + 1, x),
+            Direction::SouthWest => (y + 1, x - 1),
+            Direction::West => (y, x - 1),
+            Direction::NorthWest => (y - 1, x - 1),
+        }
+    }
+
+    pub fn iterator() -> Iter<'static, Direction> {
+        static DIRECTIONS: [Direction; 8] = [
+            Direction::North,
+            Direction::NorthEast,
+            Direction::East,
+            Direction::SouthEast,
+            Direction::South,
+            Direction::SouthWest,
+            Direction::West,
+            Direction::NorthWest,
+        ];
+
+        DIRECTIONS.iter()
+    }
 }
 
-impl FillSinks {
-    pub fn new(array: ndarray::Array2<f64>) -> Self {
-        Self { array }
-    }
+fn is_boundary(pos: (usize, usize), shape: (usize, usize)) -> bool {
+    let (y, x) = pos;
+    let (height, width) = shape;
 
-    pub fn execute() {
-        todo!()
-    }
 
-    fn dir(x: i64, y: i64, z: f64) -> Direction {
-        todo!()
-    }
+    y == 0 || x == 0 || y == height - 1 || x == width - 1
 }
 
+fn is_in_array(pos: (i64, i64), shape: (usize, usize)) -> bool {
+    let (y, x) = pos;
+    let (height, width) = shape;
+
+    y < height.try_into().unwrap() && x < width.try_into().unwrap() && y >= 0 && x >= 0
+}
+
+pub fn fill_sinks_wl(
+    elevation: &ndarray::Array2<f64>,
+    minimum_slope: f64,
+    nodata: f64,
+) -> ndarray::Array2<f64> {
+    let mut queue: std::collections::BinaryHeap<Node> = std::collections::BinaryHeap::new();
+    let shape = (elevation.shape()[0], elevation.shape()[1]);
+
+    let mut filled = ndarray::Array2::from_elem(shape, nodata);
+
+    // BOUNDARY CELLS
+    for (pos, z) in elevation.indexed_iter() {
+        if !is_boundary(pos, shape) && *z != nodata {
+            continue;
+        }
+
+        let (y, x) = pos;
+
+        queue.push(
+            Node::new(
+                y as i64,
+                x as i64,
+                *z,
+            )
+        );
+
+        filled[[y, x]] = *z;
+    }
+
+    // QUEUE
+    while !queue.is_empty() {
+        // should be fine to unwrap, we just checked if the queue is empty
+        let node = queue.pop().unwrap();
+
+        let y = node.y;
+        let x = node.x;
+        let z = filled[[y as usize, x as usize]];
+
+        for dir in Direction::iterator() {
+            let (dy, dx) = dir.delta((y as i64, x as i64));
+
+            if !is_in_array((dy, dx), shape) {
+                continue;
+            }
+
+            if elevation[[dy as usize, dx as usize]] == nodata {
+                continue;
+            }
+
+            if filled[[dy as usize, dx as usize]] != nodata {
+                continue;
+            }
+
+            let mut dz = elevation[[dy as usize, dx as usize]];
+
+            if dz < z {
+                dz = z;
+            }
+
+            queue.push(
+                Node::new(
+                    dy,
+                    dx,
+                    dz,
+                )
+            );
+
+            filled[[dy as usize, dx as usize]] = dz;
+        }
+    }
+
+    filled
+}
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
 use std::path::Path;
 
 use clap::{CommandFactory, Parser};
+use fillsinkswl::fillsinkswl::fill_sinks_wl;
 
-// TODO: set GDAL error handler
+pub fn error_handler(class: gdal::errors::CplErrType, number: i32, message: &str) {
+    let class_str = match class {
+        gdal::errors::CplErrType::None => "[INFO]",
+        gdal::errors::CplErrType::Debug => "[DEBUG]",
+        gdal::errors::CplErrType::Warning => "[WARN]",
+        gdal::errors::CplErrType::Failure => "[ERROR]",
+        gdal::errors::CplErrType::Fatal => "[FATAL]",
+    };
+
+    let msg = format!("GDAL: {class_str}, [{number}] {message}");
+
+    match class {
+        gdal::errors::CplErrType::None => {
+            println!("{msg}");
+        },
+        gdal::errors::CplErrType::Debug => {
+            println!("{msg}");
+        },
+        gdal::errors::CplErrType::Warning => {
+            println!("{msg}");
+        },
+        gdal::errors::CplErrType::Failure => {
+            eprintln!("{msg}");
+            panic!("Received a GDAL error, terminating program!");
+        },
+        gdal::errors::CplErrType::Fatal => {
+            eprintln!("{msg}");
+            panic!("Received a fatal GDAL error, terminating program!");
+        },
+    }
+}
 
 #[derive(Parser)]
 #[command(arg_required_else_help = true)]
@@ -11,29 +42,43 @@ struct Cli {
     input: String,
     output: String,
 
-    // TODO: should be > 0
-    #[arg(long = "minimum-slope", value_name = "MINIMUM_SLOPE", help = "Minimum Slope in degrees", long_help = "FIXME:")]
+    #[arg(long = "minimum-slope", value_name = "MINIMUM_SLOPE", help = "Minimum Slope in degrees")]
     minslope: Option<f64>,
 
-    #[arg(long = "overwrite", value_name = "OVERWRITE", help = "Allow overwriting output file", long_help = "FIXME:")]
+    #[arg(long = "overwrite", value_name = "OVERWRITE", help = "Allow overwriting output file")]
     overwrite: bool,
+}
+
+fn show_short_usage() {
+    match Cli::command().print_help() {
+        Ok(_) => (),
+        Err(err) => {
+            eprintln!("ERROR: Could not print usage: {err}");
+        },
+    }
 }
 
 fn main() {
     let cli = Cli::parse();
     let input = Path::new(cli.input.as_str());
     let output = Path::new(cli.output.as_str());
-    let minslope = cli.minslope.unwrap_or(0.1);
+    let minslope = cli.minslope.unwrap_or(0.0);
+
+    if minslope < 0. {
+        eprintln!("ERROR: minimum slope has to be zero or above.");
+        show_short_usage();
+        return ();
+    }
 
     if !cli.overwrite && output.exists() {
-        println!("ERROR: output already exists. You can choose to overwrite with the --overwrite option.\n");
-        Cli::command().print_help().unwrap();
+        eprintln!("ERROR: output already exists. You can choose to overwrite with the --overwrite option.\n");
+        show_short_usage();
         return ();
     }
 
     if !input.exists() {
-        println!("ERROR: input file does not exist.\n");
-        Cli::command().print_help().unwrap();
+        eprintln!("ERROR: input file does not exist.\n");
+        show_short_usage();
         return ();
     }
 
@@ -49,44 +94,110 @@ fn main() {
     let ds = match gdal::Dataset::open_ex(input, options) {
         Ok(ds) => ds,
         Err(err) => {
-            panic!("ERROR: {}", err.to_string());
+            eprintln!("ERROR: could not open input dataset: {err}");
+            show_short_usage();
+            return ();
         },
     };
 
     if ds.raster_count() != 1 {
-        println!("ERROR: input must have exactly 1 band.");
+        eprintln!("ERROR: input must have exactly 1 band.");
+        show_short_usage();
+        return ();
     }
 
     if let Some(out_driver) = gdal::DriverManager::get_output_driver_for_dataset_name(output, gdal::DriverType::Raster) {
-        let in_band: gdal::raster::RasterBand = ds.rasterband(1).unwrap();
-        let in_buffer: gdal::raster::Buffer<f64> = in_band.read_band_as().unwrap();
-        let in_crs = ds.spatial_ref().unwrap();
-        let in_array: ndarray::Array2<f64> = in_buffer.to_array().unwrap();
+        let in_band: gdal::raster::RasterBand = match ds.rasterband(1) {
+            Ok(band) => band,
+            Err(err) => {
+                eprintln!("ERROR: Could not read rasterband: {err}");
+                show_short_usage();
+                return ();
+            },
+        };
+        let in_buffer: gdal::raster::Buffer<f64> = match in_band.read_band_as() {
+            Ok(buffer) => buffer,
+            Err(err) => {
+                eprintln!("ERROR: Could not read band into a buffer: {err}");
+                show_short_usage();
+                return ();
+            },
+        };
+
+        let mut in_array: ndarray::Array2<f64> = match in_buffer.to_array() {
+            Ok(array) => array,
+            Err(err) => {
+                eprintln!("ERROR: Could not buffer into an ndarray: {err}");
+                show_short_usage();
+                return ();
+            },
+        };
 
         let width = in_array.shape()[1];
         let height = in_array.shape()[0];
 
-        let mut out_ds = out_driver.create_with_band_type::<f64, _>(
-            output,
-            width,
-            height,
-            1,
-        ).unwrap();
+        let mut out_ds = match out_driver.create_with_band_type::<f64, _>(output, width, height, 1) {
+            Ok(ds) => ds,
+            Err(err) => {
+                eprintln!("ERROR: Could not create output dataset: {err}!");
+                show_short_usage();
+                return ();
+            },
+        };
 
-        out_ds.set_spatial_ref(&in_crs).unwrap();
-        let mut out_band = out_ds.rasterband(1).unwrap();
-        let mut out_buffer = gdal::raster::Buffer::new(
-            (width, height), in_array.into_raw_vec_and_offset().0
+        match ds.spatial_ref() {
+            Ok(crs) => {
+                match out_ds.set_spatial_ref(&crs) {
+                    Ok(_) => (),
+                    Err(err) => {
+                        println!("WARNING: Could not set output CRS: {err}");
+                    },
+                }
+            },
+            Err(err) => {
+                println!("WARNING: Could not read input CRS: {err}");
+                println!("WARNING: Output CRS not set!");
+            },
+        }
+
+        let nodata = match in_band.no_data_value() {
+            Some(value) => value,
+            None => {
+                println!("WARNING: Could not read nodata value from input. Using -9999 instead.");
+                -9999.
+            },
+        };
+
+        let out_array = fill_sinks_wl(
+            &mut in_array,
+            minslope,
+            nodata,
         );
 
-        out_band.write(
-            (0, 0),
-            (width, height),
-            &mut out_buffer,
-        ).unwrap();
+        let mut out_band = match out_ds.rasterband(1) {
+            Ok(band) => band,
+            Err(err) => {
+                eprintln!("ERROR: Could not get output rasterband: {err}!");
+                show_short_usage();
+                return ();
+            },
+        };
+
+        let mut out_buffer = gdal::raster::Buffer::new(
+            (width, height), out_array.into_raw_vec_and_offset().0
+        );
+
+        match out_band.write((0, 0), (width, height), &mut out_buffer) {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("ERROR: Could not write output buffer to rasterband: {err}!");
+                show_short_usage();
+                return ();
+            },
+        }
     } else {
-        println!("ERROR: could not determine an appropriate raster driver from {}", output.to_str().unwrap());
-        Cli::command().print_help().unwrap();
+        eprintln!("ERROR: could not determine an appropriate raster driver from {}", output.to_str().unwrap_or("UNKNOWN OUTPUT"));
+        show_short_usage();
         return ();
     }
 
